02e680476a0072120357c60b74f9d9dcca43a270
Cormac McGrath <cormacmcgrath1@hotmail.com>
2019-10-23 11:12:00 +0100

    Add building.sql

---
+++ Tables/Building.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `building` (
+	`buildingId` INT NOT NULL AUTO_INCREMENT,
+	`address_houseNo` varchar(50) NOT NULL,
+	`address_addressLine1` varchar(255) DEFAULT NULL,
+	`address_addressLine2` varchar(255) DEFAULT NULL,
+	`address_city` varchar(255) DEFAULT NULL,
+	`postcode` varchar(8) DEFAULT NULL,
+PRIMARY KEY (`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
