d6e115f7309a8a470b1a09fd55884f28b85d8bf4
Cormac McGrath <cormacmcgrath1@hotmail.com>
2019-11-11 15:12:37 +0000

    Update null inserts

--- Full Scripts/Inserts.sql
+++ Full Scripts/Inserts.sql
@@ -9,14 +9,14 @@ INSERT IGNORE INTO `building` (`buildingId`, `address_houseNo`,
 VALUES  
 	(1, '4317', 'Stang', '8294 Steensland Circle', 'Tidaholm', '522 24'),
 	(2, '4', 'Barby', '9244 Graedel Avenue', 'Briteiros Santa Leocádia', '4805-458'),
-	(3, '1373', 'Quincy', '95930 Roxbury Point', 'Shangdundu', null),
-	(4, '6', 'Fairview', '4 Logan Road', 'Xiaxindian', null),
-	(5, '0', 'Forster', '16 Judy Drive', 'El Hajeb', null),
+	(3, '1373', 'Quincy', '95930 Roxbury Point', 'Shangdundu', '4805-458'),
+	(4, '6', 'Fairview', '4 Logan Road', 'Xiaxindian', '4805-458'),
+	(5, '0', 'Forster', '16 Judy Drive', 'El Hajeb', '4805-458'),
 	(6, '00', 'Ramsey', '78961 Fisk Road', 'Horní Libina', '789 74'),
-	(7, '1021', 'Maple', '2138 Sauthoff Junction', 'Maralal', null),
+	(7, '1021', 'Maple', '2138 Sauthoff Junction', 'Maralal', '4805-458'),
 	(8, '1905', 'Barby', '937 Goodland Road', 'Schenectady', '12305'),
-	(9, '2321', 'Thompson', '1766 Scoville Junction', 'Bački Petrovac', null),
-	(10, '6', 'Valley Edge', '25 Blackbird Court', 'Arma', null);
+	(9, '2321', 'Thompson', '1766 Scoville Junction', 'Bački Petrovac', '4805-458'),
+	(10, '6', 'Valley Edge', '25 Blackbird Court', 'Arma', '4805-458');
     
 -- Apartment
 INSERT IGNORE INTO `apartment` (`apartmentNo`,`buildingId`,
