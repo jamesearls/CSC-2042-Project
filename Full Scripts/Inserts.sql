b52fde8611c555b2f406abc6fc3447a936e08c54
Cormac McGrath <cormacmcgrath1@hotmail.com>
2019-11-11 15:03:25 +0000

    update inserts

--- Full Scripts/Inserts.sql
+++ Full Scripts/Inserts.sql
@@ -4,7 +4,9 @@ SET FOREIGN_KEY_CHECKS=0;
 USE `csc2042-g49`;
 
 -- Building
-INSERT IGNORE INTO `building` (`buildingId`, `address_houseNo`, `address_addressLine1`, `address_addressLine2`, `address_city`, `postcode`) values  
+INSERT IGNORE INTO `building` (`buildingId`, `address_houseNo`, 
+	`address_addressLine1`, `address_addressLine2`, `address_city`, `postcode`)
+VALUES  
 	(1, '4317', 'Stang', '8294 Steensland Circle', 'Tidaholm', '522 24'),
 	(2, '4', 'Barby', '9244 Graedel Avenue', 'Briteiros Santa Leocádia', '4805-458'),
 	(3, '1373', 'Quincy', '95930 Roxbury Point', 'Shangdundu', null),
@@ -17,7 +19,9 @@ INSERT IGNORE INTO `building` (`buildingId`, `address_houseNo`, `address_address
 	(10, '6', 'Valley Edge', '25 Blackbird Court', 'Arma', null);
     
 -- Apartment
-INSERT IGNORE INTO `apartment` (`apartmentNo`,`buildingId`,`managerId`,`noOfBedrooms`,`noOfBathrooms`,`area`) VALUES
+INSERT IGNORE INTO `apartment` (`apartmentNo`,`buildingId`,
+	`managerId`,`noOfBedrooms`,`noOfBathrooms`,`area`)
+VALUES
 	(1,1,1,2,2,46.74),
 	(2,1,2,2,1,33.21),
 	(3,1,1,1,1,53.2),
@@ -28,15 +32,16 @@ INSERT IGNORE INTO `apartment` (`apartmentNo`,`buildingId`,`managerId`,`noOfBedr
 	(3,3,1,1,1,47.1);
 
 -- Skill
-INSERT IGNORE INTO `skill` (`skillId`,`type`) VALUES 
+INSERT IGNORE INTO `skill` (`skillId`,`type`)
+VALUES 
 	(1,'Electrical'),
 	(2,'Carpentry'),
 	(3,'Plumbing');
     
 -- GuestTenant
-INSERT IGNORE INTO guestTenant 
-    (guestTenantId, duration, apartmentNo, buildingId)
-    VALUES 
+INSERT IGNORE INTO `guestTenant` 
+    (`guestTenantId`, `duration`, `apartmentNo`, `buildingId`)
+VALUES 
     (1,10,1,1),
     (2,15,2,2),
     (3,12,3,3),
@@ -44,9 +49,9 @@ INSERT IGNORE INTO guestTenant
     (5,5,5,5);
 
 -- Manager
-INSERT IGNORE INTO manager
-	(managerId, employeeID, apartmentNo, buildingId)
-    VALUES
+INSERT IGNORE INTO `manager`
+	(`managerId`, `employeeID`, `apartmentNo`, `buildingId`)
+VALUES
     (1, 10, 1, 1),
     (2, 11, 2, 1),
     (3, 12, 3, 1),
@@ -54,9 +59,9 @@ INSERT IGNORE INTO manager
     (5, 14, 5, 2);
 
 -- Tenant
-INSERT IGNORE INTO tenant 
-    (tenantId, peopleId, bankAccountNo, isActive)
-    VALUES 
+INSERT IGNORE INTO `tenant` 
+    (`tenantId`, `peopleId`, `bankAccountNo`, `isActive`)
+VALUES 
     (1,1,0293846382,1),
     (2,2,0382628618,1),
     (3,3,0309382717,1),
@@ -64,13 +69,21 @@ INSERT IGNORE INTO tenant
     (5,5,0543284739,0);
 
 -- LeaseTenant
-INSERT IGNORE INTO leaseTenant 
-    (tenantId, leaseAgreementId)
-    VALUES 
+INSERT IGNORE INTO `leaseTenant` 
+    (`tenantId`, `leaseAgreementId`)
+VALUES 
     (1,1),
     (2,1),
     (3,2),
     (4,4),
     (5,5);
+    
+INSERT INTO `leaseAgreement` (`leaseAgreementId`,`managerId`,`startDate`,
+	`duration`,`monthlyRent`,`buildingId`,`apartmentNo`,`isActive`)
+VALUES
+	(1,3,'2001-10-19 00:00:00',6,250,10,3,'1'),
+	(2,1,'2001-10-19 00:00:00',12,300,1,1,'1'),
+	(3,2,'2001-10-19 00:00:00',3,220,2,2,'1'),
+	(4,5,'2001-10-19 00:00:00',6,200,3,1,'1');
     
 SET FOREIGN_KEY_CHECKS=1;
