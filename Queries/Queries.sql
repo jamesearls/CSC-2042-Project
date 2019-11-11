9785ee50c4e6dd4613f7932dfa83c84b22521d56
40206210 <jearls01@qub.ac.uk>
2019-11-11 14:41:52 +0000

    Updated my query

--- Queries/Queries.sql
+++ Queries/Queries.sql
@@ -24,13 +24,19 @@ WHERE
         AND la.isActive = 1;
 
 -- Create a query to find the number of active tenants in each building and display the results under a column named "Number of Tennants"
-SELECT building.address_addressLine1 AS "Building", COUNT(leaseTenant.tenantId) AS "Number of Tenants" 
-FROM leaseTenant 
-	INNER JOIN leaseAgreement ON leaseTenant.leaseAgreementId = leaseAgreement.leaseAgreementId 
-	INNER JOIN building ON leaseAgreement.buildingId = building.buildingId 
-WHERE leaseAgreement.isActive = true 
-	GROUP BY leaseAgreement.buildingId 
-	ORDER BY building.address_addressLine1;
+SELECT 
+    building.address_addressLine1 AS 'Building',
+    COUNT(leaseTenant.tenantId) AS 'Number of Tenants'
+FROM
+    leaseTenant
+        INNER JOIN
+    leaseAgreement ON leaseTenant.leaseAgreementId = leaseAgreement.leaseAgreementId
+        INNER JOIN
+    building ON leaseAgreement.buildingId = building.buildingId
+WHERE
+    leaseAgreement.isActive = TRUE
+GROUP BY leaseAgreement.buildingId
+ORDER BY building.address_addressLine1;
 -- First, I have selected the building addresses to show the building that the Tenants are staying in and used the AS key word to
 -- make the attribute headings more user friendly. I then joined leaseAgreement to leaseTenant  in order to find Tenants whose 
 -- leases are active and building to leaseAgreement in order to retrieve the address of the building. I have also grouped the
